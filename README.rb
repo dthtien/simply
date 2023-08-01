1a718b6af15c128f2b792485645d2d638d86abeb
Dau The Tien <tiendt2311@gmail.com>
2023-08-01 13:30:53 +1000

    application(readme): add guide

--- README.rb
+++ README.rb
@@ -3,6 +3,15 @@
 This is a pet project where I built a website without relying on any Ruby web framework as dependencies.
 - Application server: [Puma](https://puma.io/)
 
+## Usage
+
+Install ruby 3.1.2 and run below commands
+```sh
+bundle
+puma
+```
+
+## Todo
 - [ ] Router
   - [x] GET
   - [ ] POST
