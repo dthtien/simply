2d9ee4edc0c72e298043a530901c698bff13e2a1
Dau The Tien <tiendt2311@gmail.com>
2023-08-01 13:29:05 +1000

    application: add readme

---
+++ README.rb
@@ -0,0 +1,18 @@
+# Simply
+
+This is a pet project where I built a website without relying on any Ruby web framework as dependencies.
+- Application server: [Puma](https://puma.io/)
+
+- [ ] Router
+  - [x] GET
+  - [ ] POST
+  - [ ] PUT
+  - [ ] PATCH
+  - [ ] DELETE
+- [x] Controller
+- [x] View
+- [ ] Model
+
+# References
+- https://www.akshaykhot.com/
+- https://puma.io
